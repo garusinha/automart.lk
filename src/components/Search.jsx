940d9a224023b3acd8ca6a460619ed1905393b4b
Garusinha <nikethasenarath@gmail.com>
2025-03-30 21:52:51 +0800

    Updated website content

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -12,7 +12,7 @@ import Data from "../shared/Data";
 
 function Search() {
   return (
-    <div className="p-2 bg-white rounded-md flex flex-row gap-2 px-2 items-center w-full relative">
+    <div className="p-2 bg-white rounded-md flex flex-row gap-1 px-2 items-center w-full relative">
       {/* Cars Select Box */}
       <Select className="inline-flex w-28">
         <SelectTrigger className="w-full outline-none border border-gray-300 shadow-none text-xs sm:text-sm p-1 sm:p-2 truncate">
@@ -34,7 +34,7 @@ function Search() {
       <Separator orientation="vertical" />
       {/* Car Makes Select Box */}
       <Select className="inline-flex w-36">
-        <SelectTrigger className="w-full outline-none border border-gray-300 shadow-none text-xs sm:text-sm p-1 sm:p-2 truncate">
+        <SelectTrigger className="w-full outline-none border border-gray-300 shadow-none text-xs sm:text-sm p-0.5 sm:p-2 truncate">
           <SelectValue placeholder="Car Makes" />
         </SelectTrigger>
         <SelectContent
@@ -56,7 +56,7 @@ function Search() {
       <Separator orientation="vertical" />
       {/* Pricing Select Box */}
       <Select className="inline-flex w-28">
-        <SelectTrigger className="w-full outline-none border border-gray-300 shadow-none text-xs sm:text-sm p-1 sm:p-2 truncate">
+        <SelectTrigger className="w-full outline-none border border-gray-300 shadow-none text-xs sm:text-sm p-0.5 sm:p-2 truncate">
           <SelectValue placeholder="Pricing" />
         </SelectTrigger>
         <SelectContent
@@ -76,7 +76,7 @@ function Search() {
         </SelectContent>
       </Select>
       {/* Search Button */}
-      <div className="text-sm sm:text-base bg-blue-900 text-white p-1 sm:p-2 rounded-full flex items-center justify-center cursor-pointer hover:bg-primary/80 transition-all duration-200 ease-in-out">
+      <div className="text-sm sm:text-base bg-blue-900 text-white p-0.5 sm:p-2 rounded-full flex items-center justify-center cursor-pointer hover:bg-primary/80 transition-all duration-200 ease-in-out">
         <CiSearch />
       </div>
     </div>
